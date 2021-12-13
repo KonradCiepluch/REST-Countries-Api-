fc5fbaa371238563a8b6edcf1773572f97465582
Konrad Ciepluch <konrad190008@wp.pl>
2021-12-13 22:36:16 +0100

    Added necessary changed due to api changed to v3 version.

--- src/components/CountryInfo/CountryInfo.js
+++ src/components/CountryInfo/CountryInfo.js
@@ -1,52 +1,118 @@
-import React from 'react';
-import { useParams } from 'react-router';
-import { useSelector } from 'react-redux';
-import { InfoWrapper, Back, Details, FlagWrapper, DetailsWrapper, Borders, StyledLink } from './CountryInfo.styles';
+import React from "react";
+import { useParams } from "react-router";
+import { useSelector } from "react-redux";
+import {
+  InfoWrapper,
+  Back,
+  Details,
+  FlagWrapper,
+  DetailsWrapper,
+  Borders,
+  StyledLink,
+} from "./CountryInfo.styles";
 
-const CountryInfo = ({darkmode}) => {
+const CountryInfo = ({ darkmode }) => {
+  const { name } = useParams();
 
-    const {name} = useParams();
+  const country = useSelector((state) =>
+    state.countries.find((country) => country.name.official === name)
+  );
 
-    const country = useSelector(state => state.countries.find(country => country.name === name));
+  let allCurrencies = "";
 
-    const countries = useSelector(state => state.countries);
+  const countries = useSelector((state) => state.countries);
 
-    const {name : countryName , population, nativeName, region, subRegion, subregion, capital, topLevelDomain, currencies, languages, flag, borders} = country;
+  const {
+    name: { official: countryName, nativeName },
+    population,
+    region,
+    subRegion,
+    subregion,
+    capital,
+    tld,
+    currencies,
+    languages,
+    flags,
+    borders,
+  } = country;
 
-    const getBorderCountries = () => {
-        const borderCountries =[];
-        for(let i = 0; i < borders.length; i++) {
-            const country = countries.find(item => item.alpha3Code === borders[i]);
-            borderCountries.push(country.name);
-        }; 
-        return borderCountries
+  const getBorderCountries = () => {
+    const borderCountries = [];
+    for (let i = 0; i < borders.length; i++) {
+      const country = countries.find((item) => item.cca3 === borders[i]);
+      borderCountries.push(country.name.official);
     }
-    
-    const borderCountries = getBorderCountries();
+    return borderCountries;
+  };
 
-    return ( 
-        <InfoWrapper darkmode={darkmode}>
-            <Back darkmode={`${darkmode}`} to='/'><i className="fas fa-long-arrow-alt-left"></i>Back</Back>
-            <FlagWrapper darkmode={darkmode}><img src={flag} alt="flag" /></FlagWrapper>
-            <Details darkmode={darkmode}>
-                <h3>{countryName}</h3>
-                <DetailsWrapper>
-                    <p><span>Native Name: </span> {nativeName}</p>
-                    <p><span>Population: </span>{population}</p>
-                    <p><span>Region: </span>{region}</p>
-                    <p><span>Sub Region: </span>{subRegion || subregion }</p>
-                    <p><span>Capital: </span>{capital}</p>
-                    <p><span>Top Level Domain: </span>{topLevelDomain}</p>
-                    <p><span>Currencies: </span>{currencies.map(item => item.code)}</p>
-                    <p><span>Languages: </span>{languages.map(item => item.name).join(', ')}</p>
-                </DetailsWrapper>
-                <Borders>
-                     <h4>Border Countries: </h4>
-                     {borderCountries.length ? borderCountries.map(item => <StyledLink darkmode={`${darkmode}`} to={`/country/${item}`} key={item}>{item}</StyledLink>) : <p>There are not any border countries</p>}
-                </Borders>
-            </Details>
-        </InfoWrapper>
-     );
-}
- 
+  const borderCountries = getBorderCountries();
+
+  for (const property in currencies) {
+    allCurrencies += currencies[property].name;
+  }
+
+  return (
+    <InfoWrapper darkmode={darkmode}>
+      <Back darkmode={`${darkmode}`} to="/">
+        <i className="fas fa-long-arrow-alt-left"></i>Back
+      </Back>
+      <FlagWrapper darkmode={darkmode}>
+        <img src={flags.svg} alt="flag" />
+      </FlagWrapper>
+      <Details darkmode={darkmode}>
+        <h3>{countryName}</h3>
+        <DetailsWrapper>
+          <p>
+            <span>Native Name: </span> {Object.values(nativeName)[0].official}
+          </p>
+          <p>
+            <span>Population: </span>
+            {population}
+          </p>
+          <p>
+            <span>Region: </span>
+            {region}
+          </p>
+          <p>
+            <span>Sub Region: </span>
+            {subRegion || subregion}
+          </p>
+          <p>
+            <span>Capital: </span>
+            {capital[0]}
+          </p>
+          <p>
+            <span>Top Level Domain: </span>
+            {tld[0]}
+          </p>
+          <p>
+            <span>Currencies: </span>
+            {allCurrencies}
+          </p>
+          <p>
+            <span>Languages: </span>
+            {Object.values(languages).join(", ")}
+          </p>
+        </DetailsWrapper>
+        <Borders>
+          <h4>Border Countries: </h4>
+          {borderCountries.length ? (
+            borderCountries.map((item) => (
+              <StyledLink
+                darkmode={`${darkmode}`}
+                to={`/country/${item}`}
+                key={item}
+              >
+                {item}
+              </StyledLink>
+            ))
+          ) : (
+            <p>There are not any border countries</p>
+          )}
+        </Borders>
+      </Details>
+    </InfoWrapper>
+  );
+};
+
 export default CountryInfo;
--- src/components/DataProvider/actions.js
+++ src/components/DataProvider/actions.js
@@ -1,55 +1,58 @@
-export const REQUEST_DATA = 'REQUEST_DATA';
-export const GET_DATA = 'GET_DATA';
-export const GET_DATA_FAILED = 'GET_DATA_FAILED';
-export const FILTER_DATA = 'FILTER_DATA';
-export const FILTER_SEARCHED = 'FILTER_SEARCHED';
-export const SEARCH_COUNTRIES = 'SEARCH_COUNTRIES';
-export const SHOW_ALL = 'SHOW_ALL';
+export const REQUEST_DATA = "REQUEST_DATA";
+export const GET_DATA = "GET_DATA";
+export const GET_DATA_FAILED = "GET_DATA_FAILED";
+export const FILTER_DATA = "FILTER_DATA";
+export const FILTER_SEARCHED = "FILTER_SEARCHED";
+export const SEARCH_COUNTRIES = "SEARCH_COUNTRIES";
+export const SHOW_ALL = "SHOW_ALL";
 
 export const requestData = () => ({
-    type: REQUEST_DATA
+  type: REQUEST_DATA,
 });
 export const getData = (data) => ({
-    type: GET_DATA, data
+  type: GET_DATA,
+  data,
 });
 
 export const getDataFailed = (error) => ({
-    type: GET_DATA_FAILED,
-    error
+  type: GET_DATA_FAILED,
+  error,
 });
 
 export const filterData = (region) => ({
-    type: FILTER_DATA,
-    region
+  type: FILTER_DATA,
+  region,
 });
 
 export const filterSearched = (region) => ({
-    type: FILTER_SEARCHED,
-    region
-})
+  type: FILTER_SEARCHED,
+  region,
+});
 
 export const searchCountries = (phrase) => ({
-    type: SEARCH_COUNTRIES,
-    phrase: phrase.toLowerCase()
+  type: SEARCH_COUNTRIES,
+  phrase: phrase.toLowerCase(),
 });
 
 export const showAll = () => ({
-    type: SHOW_ALL,
+  type: SHOW_ALL,
 });
 
-const url = 'https://restcountries.eu/rest/v2/all';
+const url = "https://restcountries.com/v3.1/all";
 
 export const getAllData = () => {
-    return dispatch => {
-        dispatch(requestData());
+  return (dispatch) => {
+    dispatch(requestData());
 
-        setTimeout(() => {
-            fetch(url)
-            .then(response => {
-                if (response.ok) {
-                    return response.json()}} )
-            .then(data => dispatch(getData(data)))
-            .catch(error => dispatch(getDataFailed(error)))
-        }, 2000);
-    }
+    setTimeout(() => {
+      fetch(url)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => dispatch(getData(data)))
+        .catch((error) => dispatch(getDataFailed(error)));
+    }, 2000);
+  };
 };
